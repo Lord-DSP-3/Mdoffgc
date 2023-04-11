3d0c6be80f7c47f4349b4d18447086ed6f510191
Rampage.heart <107468577+Lord-DSP-3@users.noreply.github.com>
2023-04-11 19:42:57 +0530

    Update start.py

--- SCHWI/modules/start.py
+++ SCHWI/modules/start.py
@@ -3,6 +3,6 @@ from pyrogram.types import*
 from Yukki import app
 
 
-@app.on_message(get_cmd("say"))
+@app.on_message(filters.command("start"))
 async def say(_, message: Message):
        await message.reply_text(text="HI")
