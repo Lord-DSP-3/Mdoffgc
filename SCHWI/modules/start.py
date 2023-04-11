cd0336dc21481871a70c5c95fc5a032a99b41f15
Shiro <87901742+Ishu-Hinata@users.noreply.github.com>
2023-04-11 22:24:18 +0530

    Update start.py

--- SCHWI/modules/start.py
+++ SCHWI/modules/start.py
@@ -1,8 +1,25 @@
-from pyrogram import*
-from pyrogram.types import*
-from SCHWI import app
+from pyrogram import filters
+from pyrogram.types import Message
+from SCHWI import app, cmd
+from HELPER.force_sub import Csubscribed, Gsubscribed
 
-
-@app.on_message(filters.command("start"))
+@app.on_message(cmd("start") & filters.private & Csubscribed & Gsubscribed)
 async def say(_, message: Message):
-       await message.reply_text(text="HI")
+       await message.reply_text(text="WELCOME")
+
+
+@app.on_message(cmd("start") & filters.private & Csubscribed)
+async def say(_, message: Message):
+       await message.reply_text(text="JOIN GROUP")
+
+
+@app.on_message(cmd("start") & filters.private & Gsubscribed)
+async def say(_, message: Message):
+       await message.reply_text(text="JOIN CHANNEL")
+
+
+@app.on_message(cmd("start") & filters.private)
+async def say(_, message: Message):
+       await message.reply_text(text="JOIN GROUP & CHANNEL")
+
+
