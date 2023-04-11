c4f531f1c0600d2b585796bef15353e423acaa26
Shiro <87901742+Ishu-Hinata@users.noreply.github.com>
2023-04-11 17:27:08 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,1 +1,10 @@
+from os import getenv
 
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+API_ID = 9855603
+API_HASH = "95d8b38bbb62d087dbf7b98abf670e78"
+BOT_TOKEN = "5876585593:AAHjSA--SUdjdrISF9Mt_LIEf-TMPPQg0jY"
