9214df1e72a290d12e5eaabf9630ab29fe4f3ea1
Vanitas <121037151+Lord-DSP@users.noreply.github.com>
2023-04-15 07:34:51 +0530

    Update An.py

--- SCHWI/modules/An.py
+++ SCHWI/modules/An.py
@@ -2,12 +2,14 @@
 from SCHWI import app as bot
 
 
+
 import os
 import requests
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 API_URL = "https://graphql.anilist.co"
+
 
 @bot.on_callback_query()
 def send_anime_info(client, callback_query):
@@ -30,23 +32,28 @@ def send_anime_info(client, callback_query):
     variables = {
         "id": int(anime_id)
     }
-    response = requests.post(API_URL, json={"query": anime_query, "variables": variables})
     try:
+        response = requests.post(API_URL, json={"query": anime_query, "variables": variables})
+        response.raise_for_status()
         anime_info = response.json().get("data", {}).get("Media")
-    except ValueError:
-        print(response.content)
+    except requests.exceptions.RequestException as e:
+        print(e)
         return
     title = anime_info.get("title", {}).get("romaji", "Unknown Title")
     cover_image_url = anime_info.get("coverImage", {}).get("medium", None)
     description = anime_info.get("description", "No description available.")
     if cover_image_url is not None:
-        cover_image_data = requests.get(cover_image_url).content
-        cover_image_file = f"cover_image_{anime_id}.jpg"
-        with open(cover_image_file, "wb") as f:
-            f.write(cover_image_data)
-        caption = f"<b>{title}</b>\n\n{description}"
-        callback_query.message.reply_photo(photo=cover_image_file, caption=caption, parse_mode="html")
-        os.remove(cover_image_file)
+        try:
+            cover_image_data = requests.get(cover_image_url).content
+            cover_image_file = f"cover_image_{anime_id}.jpg"
+            with open(cover_image_file, "wb") as f:
+                f.write(cover_image_data)
+            caption = f"<b>{title}</b>\n\n{description}"
+            callback_query.message.reply_photo(photo=cover_image_file, caption=caption, parse_mode="html")
+            os.remove(cover_image_file)
+        except requests.exceptions.RequestException as e:
+            print(e)
+            callback_query.answer("Error: Failed to fetch cover image.", show_alert=True)
     else:
         callback_query.answer("No cover image found for this anime.", show_alert=True)
 
@@ -67,11 +74,12 @@ def search_anime(client, message):
     variables = {
         "search": query
     }
-    response = requests.post(API_URL, json={"query": search_query, "variables": variables})
     try:
+        response = requests.post(API_URL, json={"query": search_query, "variables": variables})
+        response.raise_for_status()
         anime_list = response.json().get("data", {}).get("Media", [])
-    except ValueError:
-        print(response.content)
+    except requests.exceptions.RequestException as e:
+        print(e)
         return
     results = []
     for anime in anime_list:
@@ -83,6 +91,12 @@ def search_anime(client, message):
                 callback_data=f"anime_id:{anime_id}"
             )
         )
-    reply_markup = InlineKeyboardMarkup([results])
-    message.reply_text("Please select an anime:", reply_markup=reply_markup)
-
+    if len(results) > 0:
+        message.reply_text(
+            text="Please select an anime:",
+            reply_markup=InlineKeyboardMarkup([results])
+        )
+    else:
+        message.reply_text(
+            text="No results found."
+        )
