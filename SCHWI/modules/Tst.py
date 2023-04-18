d67c7ff8ac5e68e0e0405564bec00d69b6fe9887
Rampage.heart <107468577+Lord-DSP-3@users.noreply.github.com>
2023-04-18 22:44:05 +0530

    Update Tst.py

--- SCHWI/modules/Tst.py
+++ SCHWI/modules/Tst.py
@@ -1,237 +1,77 @@
 from SCHWI import app as bot
 from pyrogram import Client, filters
-import requests
+from config import ADMINS
+import pymongo, os
 
-# Command to search for an anime and get its info
-@bot.on_message(filters.command(["search"]))
-def search_anime(client, message):
-    # Get the anime name from the command arguments
-    args = message.text.split()
-    if len(args) < 2:
-        message.reply_text("Please provide an anime name.")
-        return
-    anime_name = " ".join(args[1:])
+SUB_ANIME_DB = "mongodb+srv://lejah82077:7hDBz80lC4sKb7EN@cluster0.jo83ynu.mongodb.net/?retryWrites=true&w=majority"
+DUB_ANIME_DB = "mongodb+srv://tasesey566:r7bEdOZnnE2lgL7H@cluster0.i17yfwi.mongodb.net/?retryWrites=true&w=majority"
+PUBLIC_C_url = "https://t.me/HORNYSOCIETY18"
+The_Other_Channel = "https://t.me/HORNYSOCIETY18"
 
-    # Build the AniList API query URL
-    query = '''
-    query ($search: String) {
-        Page {
-            media(search: $search, type: ANIME) {
-                id
-                title {
-                    romaji
-                    english
-                    native
-                }
-            }
-        }
-    }
-    '''
-    variables = {"search": anime_name}
-    url = "https://graphql.anilist.co"
-    response = requests.post(url, json={"query": query, "variables": variables})
+dbclient1 = pymongo.MongoClient(SUB_ANIME_DB)
+database1 = dbclient1["SUB_ANIME"]
+sub_anime = database1['Anime_list']
 
-    # Check if the API request was successful
-    if response.status_code != 200:
-        message.reply_text("Failed to get anime info.")
-        return
+async def present_sub_anime(anime_id : int):
+    found = sub_anime.find_one({'_id': anime_id})
+    return bool(found)
 
-    # Parse the API response and format the message
-    data = response.json()["data"]
-    anime_list = data["Page"]["media"]
-    if not anime_list:
-        message.reply_text(f"No anime found with the name '{anime_name}'.")
-        return
+async def add_sub_anime(anime_id: int, link: str):
+    sub_anime.insert_one({'_id': anime_id, '_link': link})
+    return
 
-    # Build the list of search results
-    message_text = f"Top 5 search results for '{anime_name}':\n\n"
-    for i, anime in enumerate(anime_list[:10]):
-        title = anime["title"]["english"] or anime["title"]["romaji"]
-        anime_id = anime["id"]
-        message_text += f"🖥️{i+1}. {title} (ID: {anime_id})\n\n"
+async def full_sub_Animebase():
+    user_docs = sub_anime.find()
+    user_ids = []
+    for doc in user_docs:
+        user_ids.append(doc['_id'])
+        
+    return user_ids
 
-    message.reply_text(message_text)
+async def del_sub_anime(anime_id: int):
+    sub_anime.delete_one({'_id': anime_id})
+    return
 
+async def get_sub_anime(anime_id : int):
+    found = sub_anime.find_one({'_id': anime_id})
+    dblink = found['_link']
+    return dblink
 
-# Command to get anime info by ID
-@bot.on_message(filters.command(["anime_info"]))
-def anime_info(client, message):
-    # Get the anime ID from the command arguments
-    args = message.text.split()
-    if len(args) < 2:
-        message.reply_text("Please provide an anime ID.")
-        return
-    anime_id = int(args[1])
-
-    # Build the AniList API query URL
-    query = '''
-    query ($id: Int) {
-        Media (id: $id, type: ANIME) {
-            id
-            title {
-                romaji
-                english
-                native
-            }
-            coverImage {
-                extraLarge
-            }
-            description
-            format
-            episodes
-            status
-            genres
-            averageScore
-            studios(isMain: true) {
-                edges {
-                    node {
-                        name
-                    }
-                }
-            }
-            startDate {
-                year
-                month
-                day
-            }
-        }
-    }
-    '''
-    variables = {"id": anime_id}
-    url = "https://graphql.anilist.co"
-    response = requests.post(url, json={"query": query, "variables": variables})
-
-    # Check if the API request was successful
-    if response.status_code != 200:
-        message.reply_text("Failed to get anime info.")
-        return
-
-    # Parse the API response and format the message
-    data = response.json()["data"]
-    anime = data["Media"]
-    if not anime:
-        message.reply_text(f"No anime found with the ID '{anime_id}'.")
-        return
-
-    title = anime["title"]["english"] or anime["title"]["romaji"]
-    cover_url = anime["coverImage"]["extraLarge"]
-    description = anime["description"]
-    format = anime["format"]
-    episodes = anime["episodes"]
-    status = anime["status"]
-    genres = ", ".join(anime["genres"])
-    average_score = anime["averageScore"]
-    studio = anime["studios"]["edges"][0]["node"]["name"]
-    start_date = f"{anime['startDate']['day']}/{anime['startDate']['month']}/{anime['startDate']['year']}"
-
-    message_text = f"<b>{title}</b>\n"
-    message_text += f"<i>{studio} - {start_date}</i>\n"
-    message_text += f"<b>Genres:</b> {genres}\n\n"
-    message_text += f"<b>Average Score:</b> {average_score}/100\n\n"
-    message_text += f"{description}"
-
-    message.reply_photo(cover_url, caption=message_text, parse_mode="HTML")
-
-
-
-# Command to get anime info by ID
-@bot.on_message(filters.command(["anime"]))
-def anime_info(client, message):
-    # Get the anime ID from the command arguments
-    args = message.text.split()
-    if len(args) < 2:
-        message.reply_text("Please provide an anime ID.")
-        return
-    anime_id = int(args[1])
-
-    # Build the AniList API query URL
-    query = '''
-    query ($id: Int) {
-        Media (id: $id, type: ANIME) {
-            id
-            title {
-                romaji
-                english
-                native
-            }
-            coverImage {
-                extraLarge
-            }
-            description
-            format
-            episodes
-            status
-            genres
-            averageScore
-            studios(isMain: true) {
-                edges {
-                    node {
-                        name
-                    }
-                }
-            }
-            startDate {
-                year
-                month
-                day
-            }
-            endDate {
-                year
-                month
-                day
-            }
-            duration
-            season
-            seasonYear
-            trailer {
-                id
-                site
-                thumbnail
-            }
-        }
-    }
-    '''
-    variables = {"id": anime_id}
-    url = "https://graphql.anilist.co"
-    response = requests.post(url, json={"query": query, "variables": variables})
-
-    # Check if the API request was successful
-    if response.status_code != 200:
-        message.reply_text("Failed to get anime info.")
-        return
-
-    # Parse the API response and format the message
-    data = response.json()["data"]
-    anime = data["Media"]
-    if not anime:
-        message.reply_text(f"No anime found with the ID '{anime_id}'.")
-        return
-
-    title = anime["title"]["english"] or anime["title"]["romaji"]
-    cover_url = anime["coverImage"]["extraLarge"]
-    description = anime["description"]
-    format = anime["format"]
-    episodes = anime["episodes"]
-    status = anime["status"]
-    genres = ", ".join(anime["genres"])
-    average_score = anime["averageScore"]
-    studio = anime["studios"]["edges"][0]["node"]["name"]
-    start_date = f"{anime['startDate']['day']}/{anime['startDate']['month']}/{anime['startDate']['year']}"
-    end_date = f"{anime['endDate']['day']}/{anime['endDate']['month']}/{anime['endDate']['year']}" if anime['endDate'] else ""
-    duration = f"{anime['duration']} mins" if anime['duration'] else ""
-    season = f"{anime['season']} {anime['seasonYear']}" if anime['season'] else ""
-    trailer_url = f"https://www.youtube.com/watch?v={anime['trailer']['id']}" if anime['trailer'] else ""
-
-    message_text = f"<b>{title}</b>\n"
-    message_text += f"<i>{studio} - {start_date} to {end_date}</i>\n"
-    message_text += f"<b>Genres:</b> {genres}\n"
-    message_text += f"<b>Format:</b> {format}\n"
-    message_text += f"<b>Episodes:</b> {episodes} Duration: {duration}\n"
-    message_text += f"<b>Status:</b> {status}\n"
-    message_text += f"<b>Average Score:</b> {average_score}\n"
-    message_text += f"Season {season}\n"
-    message_text += f"Start: {start_date}  End {end_date} \n\n"
-    message_text += f"DESCRIPTION: {description}"
-
-    message.reply_text(message_text)
-    message.reply_photo(cover_url, caption=message_text)
+@Bot.on_message(filters.command("addsub") & filters.user(ADMINS))
+async def add_sub(client, message):
+    if message.reply_to_message:
+        link = message.reply_to_message.text
+        if len(message.command) != 1:
+            text = message.text.split(None, 1)[1]
+            anime_id = int(text) 
+            if not await present_sub_anime(anime_id):
+                try:
+                    await add_sub_anime(anime_id, link)
+                    await message.reply_text(f"<b>ADDED!</b>\n\nID: <b>{anime_id}</b>\nLINK: {link}")
+                except Exception as e:
+                    await message.reply_text(f"An Error Occured//-\n\n{e}")
+            else:
+                dblink = await get_sub_anime(anime_id)
+                await message.reply_text(f"<b>THIS ANIME ALREDY EXIST</b>\n\nID: <b>{anime_id}</b>\n<b>POST LINK:</b> {dblink}")
+        else:
+            await message.reply_text("<b>BISH PROVIDE ANIME ID AFTER COMMAND</b>\nTo Get Anime Id \nUse Command: /anime or /search")
+    else:
+        await message.reply_text(f"Bish Reply To Post Link From Channel:\n {PUBLIC_C_url}")
+        
+    
+@Bot.on_message(filters.command("delsub") & filters.user(ADMINS))
+async def add_sub(client, message):
+    if len(message.command) != 1:
+        text = message.text.split(None, 1)[1]
+        anime_id = int(text) 
+        if await present_sub_anime(anime_id):
+            try:
+                dblink = await get_sub_anime(anime_id)
+                await del_sub_anime(anime_id)
+                await message.reply_text(f"<b>DELETED!</b>\n\nID: <b>{anime_id}</b>\n<b>POST LINK:</b> {dblink}")
+            except Exception as e:
+                await message.reply_text(f"An Error Occured//-\n\n{e}")
+        else:
+            await message.reply_text(f"No Such Anime Was Inserted In DataBase With ID: {anime_id}")
+    else:
+        await message.reply_text("<b>BISH PROVIDE ANIME ID AFTER COMMAND</b>\nTo Get Anime Id \nUse Command: /anime or /search")
