02f4738c6e6019d190fc6b7fb2d42f072fea8cb2
Rampage.heart <107468577+Lord-DSP-3@users.noreply.github.com>
2023-04-23 19:52:49 +0530

    Update Tst.py

--- SCHWI/modules/Tst.py
+++ SCHWI/modules/Tst.py
@@ -3,7 +3,97 @@ from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 from SCHWI import app
 
-# Define the /anime command
+
+async def search_find_anime_list(anime_name: str, message):
+    
+    query = '''
+        query ($search: String) {
+            Page {
+                media(search: $search, type: ANIME) {
+                    id
+                    title {
+                        romaji
+                        english
+                        native
+                    }
+                    episodes
+                    duration
+                    status
+                    bannerImage
+                }
+            }
+        }
+    '''
+    variables = {"search": anime_name}
+    url = "https://graphql.anilist.co"
+    response = requests.post(url, json={"query": query, "variables": variables})
+
+   
+    if response.status_code != 200:
+        await message.reply("API ERROR")
+        return 
+    
+    data = response.json()["data"]
+    anime_list = data["Page"]["media"]
+    if not anime_list:
+        await message.reply("Not Found")
+        return
+
+    banner_image = None
+    if len(anime_list) == 1:
+        banner_image = anime_list[0]["bannerImage"]
+    else:
+        banner_images = [anime["bannerImage"] for anime in anime_list if anime["bannerImage"]]
+        if banner_images:
+            banner_image = random.choice(banner_images)
+
+    message_text = "TOP 4 SEARCH RESULTS:"
+    buttons = []
+    for i, anime in enumerate(anime_list[:4]):
+        title = anime["title"]["english"] or anime["title"]["romaji"]
+        anime_id = anime["id"]
+        buttons.append([InlineKeyboardButton(f"🖥️{title}", callback_data=f"Anime_{anime_id}")])
+        F_B = InlineKeyboardMarkup(buttons)
+        return message_text, banner_image, F_B
+
+
+
+
+@app.on_message(filters.command("anime"))
+async def anime_search(client, message):
+    H = message.chat.id
+    args = message.text.split()
+    if len(args) < 2:
+        await message.reply_text("Please provide an anime name or ID after the command")
+        return
+
+    arg = args[1]
+    if arg.isdigit():
+        anime_id = int(arg)
+        await message.reply_text(f"Id: {anime_id}")
+    else:
+        anime_name = arg
+        message_text, banner_image, F_B = search_find_anime_list(anime_name, message)
+        await app.send_photo(H, photo=banner_image, caption=message_text, reply_markup=F_B)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 @app.on_message(filters.command("anime"))
 async def anime_search(client, message):
 
@@ -14,41 +104,32 @@ async def anime_search(client, message):
 
     arg = args[1]
     if arg.isdigit():
-        anime_id = int(arg)
+        try:
+            anime_id = int(arg)
+        except Exception as e:
+            await message.reply_text(e)
+        await message.reply_text(anime_id)
     else:
-        string = arg
+        try:
+            anime_name = arg
+        except Exception as e:
+            await message.reply_text(e)
+        await message.reply_text(anime_name)
 
-    # Set up the query to send to the AniList API
-    search_query = '''
-    query ($search: String) {
-      Media(search: $search, type: ANIME, sort: SEARCH_MATCH) {
-        id
-        title {
-          romaji
-        }
-      }
-    }
-    '''
 
-    # Set up the query variables with the user's search query
-    variables = {
-        'search': query
-    }
 
-    # Send the query to the AniList API and get the response
-    response = requests.post(search_url, json={'query': search_query, 'variables': variables}, headers={'Authorization': f'Bearer {access_token}'})
 
-    # Parse the response and get the top 4 matching anime
-    anime_list = response.json()['data']['Media']['title']
-    anime_results = []
-    for anime in anime_list[:4]:
-        anime_id = response.json()['data']['Media']['id']
-        anime_title = anime['romaji']
-        anime_results.append(f"{anime_title} (ID: {anime_id})")
 
-    # Send the results back to the user
-    if anime_results:
-        reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton(f"Watch {anime_results[0]}", url=f"https://anilist.co/anime/{anime_id}")]])
-        await message.reply_text(f"Here are the top 4 results for '{query}':\n\n" + "\n".join(anime_results), reply_markup=reply_markup)
-    else:
-        await message.reply_text("Sorry, no results found for your query.")
+
+
+
+
+
+
+
+
+
+
+
+
+
