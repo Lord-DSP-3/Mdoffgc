3d238c148ef102e7f042f4ab3a80b105fd385637
Hyuu <miyamuraizumi3dsp@gmail.com>
2023-10-31 21:55:22 +0530

    Update eval.py

--- SCHWI/modules/eval.py
+++ SCHWI/modules/eval.py
@@ -10,7 +10,7 @@ from time import time
 from pyrogram import filters
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
 
-from SCHWI import app
+from SCHWI import app, APP
 from config import ADMINS as OWNER_ID
 
 
