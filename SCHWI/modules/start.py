39a4c3f63727fa2d0972cf57a4e46a22b2ad0fa3
Hyuu <miyamuraizumi3dsp@gmail.com>
2023-10-31 18:59:13 +0530

    Update start.py

--- SCHWI/modules/start.py
+++ SCHWI/modules/start.py
@@ -1,6 +1,18 @@
 from pyrogram import filters
 from pyrogram.types import Message
 from SCHWI import app, cmd, BOT1_ID
+from SCHWI.database.redisDB_1 import pubsub
+
+for message in pubsub.listen():
+    if message['type'] == 'message':
+        # Handle the received message
+        key = message['data'].decode('utf-8')
+        await app.send_message(
+            5912572748,
+            f"Got Key: {key}"
+        )
+
+
 
 @app.on_message(cmd("pyro") & filters.private)
 async def say(_, message: Message):
