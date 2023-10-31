01c78c8e58fdf6b008c2d1d8583bf217c9c1c82f
Rampage.heart <107468577+Lord-DSP-3@users.noreply.github.com>
2023-10-31 17:26:55 +0530

    Update start.py

--- SCHWI/modules/start.py
+++ SCHWI/modules/start.py
@@ -2,24 +2,8 @@ from pyrogram import filters
 from pyrogram.types import Message
 from SCHWI import app, cmd
 
-@app.on_message(cmd("start") & filters.private)
+@app.on_message(cmd("pyro") & filters.private)
 async def say(_, message: Message):
        await message.reply_text(text="WELCOME")
-
-
-@app.on_message(cmd("start") & filters.private)
-async def say(_, message: Message):
-       await message.reply_text(text="JOIN GROUP")
-
-
-@app.on_message(cmd("start") & filters.private)
-async def say(_, message: Message):
-       await message.reply_text(text="JOIN CHANNEL")
-
-
-@app.on_message(cmd("start") & filters.private)
-async def say(_, message: Message):
-       await message.reply_text(text="JOIN GROUP & CHANNEL")
-
 
 
