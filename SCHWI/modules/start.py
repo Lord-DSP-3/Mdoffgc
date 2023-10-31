f129ab4e204438b6f06f4a28730f989cba423877
Rampage.heart <107468577+Lord-DSP-3@users.noreply.github.com>
2023-10-31 17:41:25 +0530

    Update start.py

--- SCHWI/modules/start.py
+++ SCHWI/modules/start.py
@@ -1,9 +1,17 @@
 from pyrogram import filters
 from pyrogram.types import Message
-from SCHWI import app, cmd
+from SCHWI import app, cmd, BOT1_ID
 
 @app.on_message(cmd("pyro") & filters.private)
 async def say(_, message: Message):
        await message.reply_text(text="WELCOME")
+
+@app.on_message(
+    filters.group 
+    & filters.user(BOT1_ID)
+    & filters.text
+)
+async def say(app, message: Message):
+       await app.send_message(5912572748, "⭐")
 
 
