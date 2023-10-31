8b51e044fa0670fb2f5fb1b7e40f7cbb454814ac
Hyuu <miyamuraizumi3dsp@gmail.com>
2023-10-31 21:41:58 +0530

    Update start.py

--- SCHWI/modules/start.py
+++ SCHWI/modules/start.py
@@ -4,7 +4,7 @@ from SCHWI import app, bot, cmd, botid
 
 @app.on_message(cmd("pyro") & filters.private)
 async def say(_, message: Message):
-       await message.reply_text(text="WELCOME")
+    await message.reply_text(text="WELCOME")
 
 @bot.on_message(
     filters.group 
@@ -12,12 +12,12 @@ async def say(_, message: Message):
     & filters.text
 )
 async def trace(bot, message: Message):
-       m = message.id
-       c = message.chat.id 
-       await bot.send_message(
-              5912572748, 
-              f"⭐: [`{c}`]   [{m}]"
-       )
+    m = message.id
+    c = message.chat.id 
+    await bot.send_message(
+        5912572748, 
+        f"⭐: [`{c}`]   [{m}]"
+    )
 
 
 
