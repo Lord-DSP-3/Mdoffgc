2dde7e225bb9ebb0039c96132ce16f46db9d5ffd
Hyuu <miyamuraizumi3dsp@gmail.com>
2023-10-31 21:51:41 +0530

    Update __init__.py

--- SCHWI/__init__.py
+++ SCHWI/__init__.py
@@ -10,6 +10,8 @@ boot = time.time()
 botid = 0
 botname = ""
 botusername = ""
+BOTID = 0
+BOTUSERNAME = ""
 
 
 from typing import Union
@@ -31,27 +33,26 @@ def parse_com(com, key):
   return r
 
 BOT1_TOKEN = "6265979983:AAHh0MVz9qRREziTedjvKUaT7E1DoCT0uGI"
-BOT1_ID = 6265979983
-STRING_1 = "AQGHFcwAh-FQ59i7najyKqvdmnbuGbn4jmKIdFzI7j-Xd1JZG-_BzzhPrLvDKuE1axSO9uzYcaZrCe5BGM2xeS4e9jCkmWq1ToEkrXaVCEKbqLyvxRwasIvDhhJ9CBl_9yvGIr7zmFD6wP_zk--NimE009ryAF3hXbX-vHm1XH0DVGxrDv_vvZvTWYMjxFbVWHIcKKdl5sPRJTGDH5cXRxh4HHAkxC_idLr2IDIH8rOLBruoEuDHeZDDvLdupAwyMQk-AjNwMMxu0xpo34sWgInJemb6smjul-S5-P4zMJKi9cAbpvEW5f63mV_KvdGLR-GrFFdch7rjZrPH0Ymim4enHLETDAAAAAE94jjSAA"
+BOT2_TOKEN = "6499811752:AAEuOKyeKmb0kltqE9rRiAQfUXXDJf7siSo"
 
 app = Client(
-    "LVLbot",
+    "LVLbot1",
     config.API_ID,
     config.API_HASH,
     bot_token=BOT1_TOKEN,
 )
 
-bot = Client(
-    name="userbot1",
-    session_string=STRING_1,
-    api_hash=config.API_HASH,
-    api_id=config.API_ID
+APP = Client(
+    "LVLbot2",
+    config.API_ID,
+    config.API_HASH,
+    bot_token=BOT2_TOKEN,
 )
 
 async def initiate_bot():
     global botid, botname, botusername
     await app.start()
-    await bot.start()
+    await APP.start()
     getme = await app.get_me()
     botid = getme.id
     botusername = (getme.username).lower()
@@ -59,5 +60,8 @@ async def initiate_bot():
         botname = getme.first_name + " " + getme.last_name
     else:
         botname = getme.first_name
+    get_me = await APP.get_me()
+    BOTID = get_me.id
+    BOTUSERNAME = (get_me.username).lower()
 
 loop.run_until_complete(initiate_bot())
