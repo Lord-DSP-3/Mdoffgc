5e26686c29812d0b7515eebf9c40a8e11b665223
Rampage.heart <107468577+Lord-DSP-3@users.noreply.github.com>
2023-10-31 16:43:16 +0530

    Update start.py

--- SCHWI/modules/start.py
+++ SCHWI/modules/start.py
@@ -1,19 +1,18 @@
 from pyrogram import filters
 from pyrogram.types import Message
 from SCHWI import app, cmd
-from HELPER.force_sub import Csubscribed, Gsubscribed
 
-@app.on_message(cmd("start") & filters.private & Csubscribed & Gsubscribed)
+@app.on_message(cmd("start") & filters.private)
 async def say(_, message: Message):
        await message.reply_text(text="WELCOME")
 
 
-@app.on_message(cmd("start") & filters.private & Csubscribed)
+@app.on_message(cmd("start") & filters.private)
 async def say(_, message: Message):
        await message.reply_text(text="JOIN GROUP")
 
 
-@app.on_message(cmd("start") & filters.private & Gsubscribed)
+@app.on_message(cmd("start") & filters.private)
 async def say(_, message: Message):
        await message.reply_text(text="JOIN CHANNEL")
 
