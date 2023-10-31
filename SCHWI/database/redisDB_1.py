318cb00266e63c847887bac73206f683593a67bf
Hyuu <miyamuraizumi3dsp@gmail.com>
2023-10-31 18:33:08 +0530

    Create redisDB_1.py

---
+++ SCHWI/database/redisDB_1.py
@@ -0,0 +1,6 @@
+import redis
+# dihabor940@wisnick.com pp_00000_DSP
+Redis_client = redis.Redis(
+  host='redis-18712.c299.asia-northeast1-1.gce.cloud.redislabs.com',
+  port=18712,
+  password='hBZT6va5IkajpOOUK7XlPDG26WS6EoqW')
